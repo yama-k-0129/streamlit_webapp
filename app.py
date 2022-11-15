fdf1a74b2f8f15d9758cdbc0c39d2f7836fc7781
yama-k-0129 <110022751+yama-k-0129@users.noreply.github.com>
2022-11-15 10:36:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -166,11 +166,11 @@ if selected == "Entry":
                         # gmailに送信
                         send_outlook_mail(msg)
                         return schedule.CancelJob()
-                    schedule.every().day.at(time_utc).do(send_tama_message)
-                    st.text(f'{name_category}さん！{time}に{switch}しました。')  
-                    while True:
-                        schedule.run_pending()
-                        sleep(31)
+                schedule.every().day.at(time_utc).do(send_tama_message)
+                st.text(f'{name_category}さん！{time}に{switch}しました。')  
+                while True:
+                    schedule.run_pending()
+                    sleep(31)
             elif name_category == '早崎水彩':
                 def send_zaki_message():
                     """
