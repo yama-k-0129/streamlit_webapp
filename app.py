b82f0a286917eb439d6d54811e88f3b32d9a59da
yama-k-0129 <110022751+yama-k-0129@users.noreply.github.com>
2022-11-15 14:33:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -117,9 +117,7 @@ if selected == "Entry":
             dt = datetime.combine(date,time)#日本時間のdatetimeオブジェクトが生成
             dt_utc = dt - timedelta(hours=9)#時差が反映され-9時間され，tzinfoが付加されたdatetimeオブジェクトが生成
             time_utc = dt_utc.time() 
-            dt_now = datetime.now()
-            deltat = dt_utc - dt_now
-            delta_seconds = deltat.total_seconds()
+            deltat = (dt_utc - datetime.now()).seconds
             time_utc = str(time_utc)
             daytime = str(date) + "_" + str(time) + "_" + str(name_category)
             name = str(name_category)
@@ -171,11 +169,7 @@ if selected == "Entry":
                         return schedule.CancelJob()
                 time.sleep(delta_seconds)
                 send_tama_message()
-#                 schedule.every().day.at(time_utc).do(send_tama_message)
-#                 st.text(f'{name_category}さん！{time}に{switch}しました。')  
-#                 while True:
-#                     schedule.run_pending()
-#                     sleep(31)
+                st.text(f'{name_category}さん！{time}に{switch}しました。') 
             elif name_category == '早崎水彩':
                 def send_zaki_message():
                     """
